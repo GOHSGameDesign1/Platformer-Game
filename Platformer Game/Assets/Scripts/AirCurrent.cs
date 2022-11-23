8dd20fa880fe38332a1241b62caba1ee365eb3de
GOHSGameDesign1 <GOHSGameDesign1@gmail.com>
2022-11-22 23:44:20 -0800

    Cleaned up some code, changed numbers

--- Platformer Game/Assets/Scripts/AirCurrent.cs
+++ Platformer Game/Assets/Scripts/AirCurrent.cs
@@ -8,10 +8,10 @@ public class AirCurrent : MonoBehaviour
 {
     [SerializeField] private bool playerInside;
 
-    [field: SerializeField] public Vector3 velocity;
-    [Tooltip("How fast the wind pushes the player (and the particles)")] public float magnitude;
-    [Tooltip("How many particles there are (divided by 10 and multiplied by width")] public float particleCount;
-    [Tooltip("The curve for the z rotation of the particles")] public AnimationCurve aniCurve;
+    [field: SerializeField] public Vector3 velocity { get; private set; }
+    [field: SerializeField][Tooltip("How fast the wind pushes the player (and the particles)")] public float magnitude { get; private set; }
+    [field: SerializeField][Tooltip("How many particles there are (divided by 10 and multiplied by width")] public float particleCount { get; private set; }
+    [field: SerializeField][Tooltip("The curve for the z rotation of the particles")] public AnimationCurve aniCurve { get; private set; }
 
     private ParticleSystem airParticles;
     // Start is called before the first frame update
@@ -21,32 +21,41 @@ public class AirCurrent : MonoBehaviour
         airParticles = transform.GetChild(0).GetComponent<ParticleSystem>();
     }
 
+    private void Start()
+    {
+        
+    }
+
     // Update is called once per frame
     void Update()
     {
         velocity = transform.up * magnitude;
 
-        var main = airParticles.main;
+        EditParticles();
+    }
 
+    void EditParticles()
+    {
+        var main = airParticles.main;
         var velLifetime = airParticles.velocityOverLifetime;
-        //velLifetime.x = transform.up.x * magnitude;
-        velLifetime.y = magnitude * 10/3;
+        var shape = airParticles.shape;
+        var emission = airParticles.emission;
+
+        float particleVelocity = magnitude * 10;
+
+        main.startLifetime = Mathf.Clamp(transform.lossyScale.y / particleVelocity * 0.9f, 0.1f, 5f);
+
+        velLifetime.y = particleVelocity;
         velLifetime.orbitalZ = new ParticleSystem.MinMaxCurve((0.007f * magnitude), aniCurve);
 
-        main.startLifetime = Mathf.Clamp(transform.lossyScale.y / (magnitude * 10 / 3) * 0.9f, 0.1f, 5f);
-
-        var shape = airParticles.shape;
-        shape.position = new Vector3(0, transform.lossyScale.y/-2 + 3, 0);
-        //transform.GetChild(0).localPosition = new Vector3(0, transform.localScale.y * -0.41f/33f, 0);
-
-        var emission = airParticles.emission;
-        emission.rateOverTime = particleCount/10f * transform.lossyScale.x;
+        shape.position = new Vector3(0, transform.lossyScale.y / -2 + 3, 0);
         shape.scale = new Vector3(transform.lossyScale.x, 6, 0);
+
+        emission.rateOverTime = particleCount / 10f * transform.lossyScale.x;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //Debug.Log("entered");
 
         if(collision.tag == "Player")
         {
@@ -66,44 +75,4 @@ public class AirCurrent : MonoBehaviour
     {
         Gizmos.DrawLine(transform.position, velocity + transform.position);
     }
-
-    /*private void OnTriggerStay2D(Collider2D collision)
-    {
-        Debug.Log("triggerd!");
-        if(TryGetComponent(out Rigidbody2D rb))
-        {
-            rb.velocity = Vector3.zero;
-        }
-    }*/
 }
-
-//Handle for the velocity vector;
-#if UNITY_EDITOR
-[CustomEditor(typeof(AirCurrent))]
-[CanEditMultipleObjects]
-public class AirCurrentEditor : Editor
-{
-    //SerializedObject linkedObject;
-    public void OnSceneGUI()
-    {
-        /*var linkedObject = target as AirCurrent;
-        //ParticleSystem.MainModule airMain = linkedObject.airParticles.main;
-
-        Handles.color = Color.yellow;
-
-        Handles.DrawLine(linkedObject.transform.position, linkedObject.velocity + linkedObject.transform.position);
-        
-        EditorGUI.BeginChangeCheck();
-
-        Vector3 newVelocity = Handles.PositionHandle(linkedObject.velocity + linkedObject.transform.position, Quaternion.identity);
-
-        if (EditorGUI.EndChangeCheck())
-        {
-            Undo.RecordObject(target, "Update position");
-            //linkedObject.velocity = newVelocity - linkedObject.transform.position;
-            //linkedObject.airParticles.main.startLifetime = 20 / linkedObject.velocity.y * 0.7f;
-
-        }*/
-    }
-}
-#endif //UNITY_EDITOR
