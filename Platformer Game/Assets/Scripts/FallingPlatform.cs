40c64573bb11337fe5f9ce11b1a8db13ae1db53e
GOHSGameDesign1 <GOHSGameDesign1@gmail.com>
2022-11-24 20:41:51 -0800

    Update FallingPlatform.cs

--- Platformer Game/Assets/Scripts/FallingPlatform.cs
+++ Platformer Game/Assets/Scripts/FallingPlatform.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class FallingPlatform : MonoBehaviour
 {
+    public float maxFallSpeed;
+
+    private Rigidbody2D rb;
+    private bool falling;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        falling = false;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            StartCoroutine(aboutToFall());
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(falling) 
+        {
+            if(Mathf.Abs(rb.velocity.y) < maxFallSpeed)
+            {
+                rb.velocity += Vector2.down / 2;
+            }
+        }
+    }
+
+    IEnumerator aboutToFall()
+    {
+        yield return new WaitForSeconds(0.2f);
+        falling = true;
     }
 }
