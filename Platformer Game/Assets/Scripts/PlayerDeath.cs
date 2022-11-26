8e9664e9f2f34fbacc3d6325f12b3e1405660991
GOHSGameDesign1 <GOHSGameDesign1@gmail.com>
2022-11-26 00:17:47 -0800

    Update PlayerDeath.cs
    Added level reset on death

--- Platformer Game/Assets/Scripts/PlayerDeath.cs
+++ Platformer Game/Assets/Scripts/PlayerDeath.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerDeath : MonoBehaviour
 {
@@ -23,5 +24,10 @@ public class PlayerDeath : MonoBehaviour
                 Destroy(gameObject);
             }
         }
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
