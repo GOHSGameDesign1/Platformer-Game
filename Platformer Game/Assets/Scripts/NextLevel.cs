cc109890ff8315d65c1898058e0a19f0d55c0d01
GOHSGameDesign1 <GOHSGameDesign1@gmail.com>
2022-11-25 23:46:18 -0800

    Update NextLevel.cs
    Null exception for levels

--- Platformer Game/Assets/Scripts/NextLevel.cs
+++ Platformer Game/Assets/Scripts/NextLevel.cs
@@ -7,6 +7,12 @@ public class NextLevel : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if(SceneManager.GetActiveScene().buildIndex == SceneManager.sceneCountInBuildSettings  - 1)
+        {
+            Debug.Log("Out of scenes");
+            return;
+        }
+
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
